e6cbf79b4792bb8cc664e71bccc7dc50fe9784a2
franbono2 <franbono2@gmail.com>
2021-12-19 12:08:25 +0100

    URI añadida

--- temp/urls.py
+++ temp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DatoCreate, DatoList
+from .views import DatoCreate, DatoList, DatoList10
 
 urlpatterns = [
     path('nuevo/', DatoCreate.as_view()),
-    path('', DatoList.as_view()),
+    path('listajson/', DatoList10.as_view()),
+    path('', DatoList.as_view())
 ]
