78445964de834ae8eb83dab26d15863e71c2ca17
franbono2 <franbono2@gmail.com>
2022-01-02 19:58:28 +0100

    retoques al compose y permitir los distintos host para los containers

--- dockerDSC/settings.py
+++ dockerDSC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qt7#473_s3k-t59a5wndb99_pgfx_n*!(gh(f_z7w4!840ssn&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
